0922821188c9d021bcd496ab54e13344b7efbf73
nissoibragimova <unisso@yahoo.com>
2021-10-03 12:56:19 -0400

    made a method

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -8,5 +8,10 @@ package setup;public class Third {
 
     public static void main(String[] args) {
         System.out.println("5");
+        m();
+    }
+    public static void m(){
+        System.out.println(6);
+
     }
 }
