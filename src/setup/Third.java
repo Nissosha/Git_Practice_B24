94269a073461cbe82bb4ec98767adbc005853d2f
nissoibragimova <unisso@yahoo.com>
2021-10-03 12:55:16 -0400

    added main method

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -3,4 +3,10 @@ package setup;public class Third {
     // some new code
 
     // set up third class
+
+    // some more code
+
+    public static void main(String[] args) {
+        System.out.println("5");
+    }
 }
