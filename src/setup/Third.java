7197802f61f2b59c4b11409d0e8c9e59435b36df
nissoibragimova <unisso@yahoo.com>
2021-10-03 12:54:28 -0400

    set up third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -1,15 +1,6 @@
-package setup;
+package setup;public class Third {
 
-public class Third {
-    public void m() {
-        System.out.println("first method");
-    }
+    // some new code
+
+    // set up third class
 }
-
-
-// This is our Third class
-
-// we are going to have some lunch soon
-
-// more commit
-
