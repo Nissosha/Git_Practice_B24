5af80213d599b38232ff1018add2125756c7207c
nissoibragimova <unisso@yahoo.com>
2021-10-03 20:14:52 -0400

    added coffeelover test

---
+++ src/Tests/CoffeeLoverTest.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class CoffeeLoverTest {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Coffee Lover");
+
+        }
+    }
+}
